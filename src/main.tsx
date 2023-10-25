54a39e3571f5ae78cfc4eeca465e6aa0de93d12b
d-tysh <39271488+d-tysh@users.noreply.github.com>
2023-10-25 18:19:12 +0300

    IGG-project

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { store } from './redux/store.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='IGG-project'>
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
