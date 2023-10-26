d3ae3eef4c8b25f0a50a85bb736e01c28cbd2e7f
d-tysh <39271488+d-tysh@users.noreply.github.com>
2023-10-26 14:11:03 +0300

    IGG-project

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,13 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/slice";
 import { tableReducer } from "./table/slice";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+
+const authPersistConfig = {
+    key: 'auth',
+    storage,
+    whitelist: ['isLoggedIn']
+}
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
+        auth: persistReducer(authPersistConfig, authReducer),
         table: tableReducer
     }
 })
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
